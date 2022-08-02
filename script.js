dd30f2b6372f502c888a5d8e183dc501db189ef6
Rene <renesuar99@gmail.com>
2022-08-02 13:48:37 -0400

    created function that adds books to library array

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
 const modalBackground = document.getElementsByClassName("bgModal");
-
 const button = document.getElementsByClassName("button")
 const closeButton = document.getElementById("close");
+
+//  form variables. 
+let title = "";
+let author = ""; 
+let pageNumbers = ""; 
+let readOrNot = ""; 
+let library = [];
+
+
+function Book(title, author, pageNumbers, readOrNot) { 
+   
+    this.title = title; 
+    this.author = author;
+    this.pageNumbers = pageNumbers;
+    this.readOrNot = readOrNot;
+    this.info = function (){
+        console.log(this.title, this.author, this.pageNumbers, readOrNot)
+    }
+}
+
+let submitButton = document.getElementById("submit");
+
+submitButton.addEventListener('click', function (e){
+    i = 0;
+    title = document.getElementById("bookName").value;
+    author = document.getElementById("authorName").value;
+    pageNumbers = document.getElementById("pageNumbers").value;
+    readOrNot = document.getElementById("readOrNot").checked;
+    library[i] = new Book (title, author, pageNumbers, readOrNot)
+    library[i].info();
+
+    
+//closes form on submit.
+modalBackground[0].style.display = "none";
+
+});
+
+
 
 
 button[0].addEventListener('click', function(e) {
